78a698824180957cc1062ddd3ec9af36cf87d8dc
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-07-04 01:33:42 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.09%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    // BRUTEFORCE
+    // OPTIMISED
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>> rotated(n,vector<int>(n,0));
-        
+        // transpose of matrix
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                rotated[j][n-i-1]=matrix[i][j];
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        
+        // REVERSE ALL THE ROWS
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=rotated[i][j];
-            }
+            reverse(matrix[i].begin(),matrix[i].end());
         }
+        
     }
 };
