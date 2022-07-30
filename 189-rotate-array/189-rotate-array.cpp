d14f012e83cceb5146e420cebf962c8f0755cb81
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-07-30 21:55:03 +0530

    Time: 75 ms (5.06%), Space: 25.4 MB (40.51%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    // T(c): O(n) 
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        k=k%n;
+        vector<int> temp(n);
+        for(int i=0;i<n;i++){
+            temp[(i+k)%n]=nums[i];
+        }
         
-        reverse(nums.begin(),nums.begin()+n-k);
-        reverse(nums.begin()+n-k,nums.end());
-        reverse(nums.begin(),nums.end());
+        nums=temp;
+        
+        // REVERSE TECHNIQUE
+//         int n=nums.size();
+//         k=k%n;
+        
+//         reverse(nums.begin(),nums.begin()+n-k);
+//         reverse(nums.begin()+n-k,nums.end());
+//         reverse(nums.begin(),nums.end());
     }
 };
