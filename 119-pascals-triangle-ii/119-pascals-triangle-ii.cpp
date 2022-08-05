6c39cd0e3f089fa4f91864538273028c2643b4e4
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-06 03:19:08 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (80.12%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    
+    long double fact(int n){
+        if(n<2) return 1;
+        return n*fact(n-1);
+    }
+        
     vector<int> getRow(int rowIndex) {
-        // ITERATIVE APPROACH
-        vector<int> curr={1},prev={1};
-        for(int i=1;i<=rowIndex;i++){
-            curr.push_back(1);
-            for(int j=1;j<i;j++){
-                curr[j]=prev[j]+prev[j-1];
-            }
-            prev=curr;
+        
+        // nCr APPROACH
+        vector<int> ans;
+        for(int i=0;i<=rowIndex;i++){
+            ans.push_back(double((fact(rowIndex)/(fact(i)*(fact(rowIndex-i))))));
         }
-        return curr;
+        return ans;
+        
+        // ITERATIVE APPROACH
+        // vector<int> curr={1},prev={1};
+        // for(int i=1;i<=rowIndex;i++){
+        //     curr.push_back(1);
+        //     for(int j=1;j<i;j++){
+        //         curr[j]=prev[j]+prev[j-1];
+        //     }
+        //     prev=curr;
+        // }
+        // return curr;
+        
         
         // RECURSIVE APPROACH
        
