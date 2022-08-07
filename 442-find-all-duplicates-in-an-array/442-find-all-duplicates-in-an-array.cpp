161df8193c710bd8cb40b7cf8e37db31cbed8060
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-07 20:19:37 +0530

    Time: 93 ms (61.36%), Space: 33.6 MB (80.11%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    // BRUTE FORCE
+    // CONSTANT SPACE we can solve using hashtable also
+    // We are using 1-n thing that is we are checking in entire array with help of  that and marking it negative 
     vector<int> findDuplicates(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
         vector<int> ans;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                ans.push_back(nums[i]);
+        for(int i=0;i<n;i++){
+            int temp=abs(nums[i]);
+            if(nums[temp-1]<0){
+                ans.push_back(temp);
             }
+            nums[temp-1]=-nums[temp-1];
         }
+        
         return ans;
     }
 };
