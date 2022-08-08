652cd541ad24724a0f4b23dbc11795622fb17f9e
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-08 18:55:52 +0530

    Time: 8 ms (43.50%), Space: 9.6 MB (51.76%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-    // BINARY SEARCH METHOD MOST OPTIMAL
+    // OPTIMAL APPROACH: BINARY SEARCH METHOD MOST OPTIMAL
     // T(c)=LOG2(N*M)
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+    bool searchMatrix(vector<vector<int>>& matrix, int target){
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int l=0,h=r*c-1;
         
-        int lo=0;
-        int mid;
-        int hi=(m*n)-1;
-        while(lo<=hi){
-            mid=(lo+(hi-lo)/2);
-            if(matrix[mid/m][mid%m]==target){
+        while(l<=h){
+            
+            int mid=l+(h-l)/2;
+            if(matrix[mid/c][mid%c]==target){
                 return true;
             }
-            if(matrix[mid/m][mid%m]<target){
-                lo=mid+1;
+            if(matrix[mid/c][mid%c]<target){
+                l=mid+1;
             }
             else{
-                hi=mid-1;
+                h=mid-1;
             }
+            
         }
+        
         return false;
     }
+
+    
+    // BETTER APPROACH
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int row=matrix.size();
+//         int r=0;
+//         int c=matrix[0].size()-1;
+//         bool flag=false;
+//         while(r<row && c>=0){
+//             if(matrix[r][c]==target){
+//                 flag=true;
+//             }
+//             if(matrix[r][c]>target){
+//                 c--;
+//             }
+//             else{
+//                 r++;
+//             }
+            
+//         }
+        
+//         return flag;
+//     }
 };
