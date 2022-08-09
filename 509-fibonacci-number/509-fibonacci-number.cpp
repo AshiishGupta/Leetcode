743b0d98637aeaf0becc245515a274727833102f
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-10 01:50:12 +0530

    Time: 3 ms (58.90%), Space: 5.7 MB (96.11%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
+    int dp[31]={0};
     int fib(int n) {
+        // USING DP
         if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        if(dp[n]){
+            return dp[n];
+        }
+        return dp[n]=fib(n-1)+fib(n-2);
+        
+        // USING RECURSION
+        // if(n==0 || n==1) return n;
+        // return fib(n-1)+fib(n-2);
     }
 };
