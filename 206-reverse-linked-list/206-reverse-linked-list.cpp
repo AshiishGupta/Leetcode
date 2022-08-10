cebbad00d3182dbbac9b4ca37aa3cb75cb400824
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-10 19:59:57 +0530

    Time: 14 ms (24.14%), Space: 8.5 MB (10.40%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -12,15 +12,12 @@ class Solution {
 public:
     // RECURSIVELY
     ListNode* reverseList(ListNode* head) {
-        
-        if(head == NULL || head->next == NULL){
+        if(head==NULL || head->next==NULL){
             return head;
         }
-        
         ListNode* newhead=reverseList(head->next);
         head->next->next=head;
         head->next=NULL;
-        
-        return newhead;
+        return newhead;        
     }
 };
