33153d03099bcf31cd4dcc1cb9a67c8707960d5d
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-11 04:16:27 +0530

    Time: 4 ms (22.32%), Space: 6.1 MB (40.46%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     // WE ARE DOING MEMOIZATION TO THAT RECURSIVE SOLN
-//     int countway(int n,vector<int> &memo){
+    int countway(int n,vector<int> &memo){
         
-//         if(n<0){
-//             return 0;
-//         }
-//         if(memo[n]!=-1) return memo[n];
-        
-//         if(n==0) return 1;
-        
-//         return memo[n]=countway(n-1,memo)+countway(n-2,memo);
-//     }
-//     int climbStairs(int n) {
-//         vector<int> memo(n+1,-1);
-//         return countway(n,memo);
-//     }
-    
-    // CONVERTING MEMOIZATION TO TABULATION OR DP
-    int climbStairs(int n){
-        if(n<0) return 0;
-        
-        int dp[100];
-        dp[0]=1;
-        dp[1]=2;
-        
-        for(int i=2;i<n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n<0){
+            return 0;
         }
-        return dp[n-1];
+        if(memo[n]!=-1) return memo[n];
+        
+        if(n==0) return 1;
+        
+        return memo[n]=countway(n-1,memo)+countway(n-2,memo);
+    }
+    int climbStairs(int n) {
+        vector<int> memo(n+1,-1);
+        return countway(n,memo);
     }
     
-    
+    // CONVERTING MEMOIZATION TO TABULATION OR DP
+//     int climbStairs(int n){
+//         if(n<0) return 0;
+        
+//         int dp[100];
+//         dp[0]=1;
+//         dp[1]=2;
+        
+//         for(int i=2;i<n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n-1];
+//     }
     
     
     // RECURSIVE
