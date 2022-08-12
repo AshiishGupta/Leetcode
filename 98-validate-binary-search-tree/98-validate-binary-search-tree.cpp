cd43b6024559042b7e6c3465f8a56bd0402eb2d7
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-12 20:16:24 +0530

    Time: 15 ms (77.77%), Space: 21.8 MB (30.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,24 +11,47 @@
  */
 class Solution {
 public:
-    void inOrder(TreeNode* root) {
-        if (!root)
-            return;
-        inOrder(root->left);
-        tree.push_back(root->val);
-        inOrder(root->right);
-    }
-   
-    bool isValidBST(TreeNode* root) {
-        if (!root)
-            return true;
+    // Using recursion-- range method ,lower range and higher range
+    
+    bool isValidBST(TreeNode*root,long low,long high){
+        if(root==NULL) return true;
         
-        inOrder(root);
-        for (int i=1; i<tree.size(); i++)
-            if (tree[i] <= tree[i-1])
-                return false;
-        return true;
+        if(root->val<high && root->val>low){
+            return isValidBST(root->left,low,root->val) && isValidBST(root->right,root->val,high);
+        }
+        else{
+            return false;
+        }
+        
     }
-private:
-    vector<int> tree;
+    bool isValidBST(TreeNode* root) {
+        long low=LONG_MIN;
+        long high=LONG_MAX;
+        return isValidBST(root,low,high);
+    }
+    
+    
+    // USING INORDER TRAVERSAL AND CHECKING IT IF IT IS VALID OR NOT 
+    // POINT HERE IS: IN INORDER TRAVERSAL OF ANY BINARY TREE IS SORTED List
+//     void inOrder(TreeNode* root) {
+//         if (!root)
+//             return;
+//         inOrder(root->left);
+//         tree.push_back(root->val);
+//         inOrder(root->right);
+        
+//     }
+   
+//     bool isValidBST(TreeNode* root) {
+//         if (!root)
+//             return true;
+        
+//         inOrder(root);
+//         for (int i=1; i<tree.size(); i++)
+//             if (tree[i] <= tree[i-1])
+//                 return false;
+//         return true;
+//     }
+// private:
+//     vector<int> tree;
 };
