b6e4c1e2183f91f4cc4e2973941654871d9cee67
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-17 20:23:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (25.21%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    // 2 POINTER APPROACH
+    // 2 pointer approach THINK OF START INDEX IN HAYSTACK 
     int strStr(string haystack, string needle) {
-        int i=0,j=0;
-        if(needle==""){
-            return 0;
-        }
+        int i=0;
+        int j=0;
+        if(!needle.size()) return 0; 
         while(i<haystack.size() && j<needle.size()){
             if(haystack[i]==needle[j]){
                 i++;
                 j++;
                 if(j==needle.size()){
-                    return i-needle.size();
+                    return i-j;
                 }
-                
             }
             else{
                 i=i-j+1;
