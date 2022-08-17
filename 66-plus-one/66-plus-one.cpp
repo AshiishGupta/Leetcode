cbfa6c6685e28f957d778fd7c9f8e451cd7a1b46
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-17 20:42:30 +0530

    Time: 5 ms (29.40%), Space: 8.6 MB (85.87%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        int carry=0;
         int n=digits.size();
-        for(int i=n-1;i>=0;i--){
-            if(digits[i]==9){
-                digits[i]=0;
-            }
-            else{
-                digits[i]+=1;
-                return digits;
-            }
+        int temp=digits[n-1]+1;
+        
+        if(temp>9){
+            digits[n-1]=temp%10;
+            carry=temp/10;
+        }
+        else{
+            digits[n-1]=temp;
         }
         
-        digits.push_back(0);
-        digits[0]=1;
+        
+        for(int i=n-2;i>=0;i--){
+            
+            if(carry!=0){
+                int x=(digits[i]+carry);
+                digits[i]=x%10;
+                carry=x/10;
+            }
+            
+        }
+        if(carry!=0){
+            digits.push_back(0);
+            digits[0]+=1;
+        }
         return digits;
     }
 };
