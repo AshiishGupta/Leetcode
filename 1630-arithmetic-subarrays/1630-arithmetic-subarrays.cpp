d8980fdc0c6f07e6d66f026563edfdec3f208f79
Ashish Gupta <59842291+AshiishGupta@users.noreply.github.com>
2022-08-29 18:35:49 +0530

    Time: 85 ms (71.70%), Space: 25.9 MB (67.33%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool> ans;
+        
+        for(int i=0;i<l.size();i++){
+            
+            if(r[i]-l[i]<2) {
+                ans.push_back(true);
+            }
+            else{
+                
+                vector<int> temp(nums.begin()+l[i],nums.begin()+r[i]+1);
+                bool flag=false;
+                sort(temp.begin(),temp.end());
+                int diff=temp[1]-temp[0];
+                for(int j=2;j<=r[i]-l[i];j++){
+                    if(temp[j]-temp[j-1]!=diff){
+                        flag=true;
+                        break;
+                    }
+                }
+                ans.push_back(!flag);
+            }
+        }
+
+        return ans;
+        
+    }
+};
